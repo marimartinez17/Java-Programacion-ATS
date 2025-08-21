33e78279778faae31bc236c98ded95d5c753337e
María Martínez <mariadanielamartinezr@gmail.com>
2025-08-21 10:49:24 -0400

    Add files via upload

---
+++ Proyecto1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1;
+
+/**
+ *
+ * @author usuario
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        byte entero1 = 127;
+        
+        System.out.println("Entero: "+entero1);
+        
+        short entero2 = 1277;
+        
+        System.out.println("Entero: "+entero2);
+        
+        int entero3 = 1277777;
+        System.out.println("Entero: "+entero3);
+        
+        long entero4 = 123478943;
+        System.out.println("Entero: "+entero4);
+        
+        float decimal1 = 12.99f;
+        System.out.println("Decimal: "+decimal1);
+        
+        double decimal2 = 12.9999999999999;
+        System.out.println("Decimal: "+decimal2);
+        
+        char caracter = 'a';
+        System.out.println("Caracter: "+caracter);
+        
+        boolean desicion = true;
+        System.out.println("Desicion: "+desicion);
+    }
+}
---
+++ Proyecto2.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.proyecto2;
+
+
+public class Proyecto2 {
+
+    public static void main(String[] args) {
+        Integer numero = null;
+        
+        System.out.println("El numero es: "+numero);
+        
+        String palabra = "Hola que tal";
+        
+        System.out.println(palabra);
+    }
+}
