aff681eb5786c81f9baa46d066ccfaf5e3629a70
María Martínez <mariadanielamartinezr@gmail.com>
2025-08-21 11:24:50 -0400

    Update Proyecto3.java

--- Proyecto3.java
+++ Proyecto3.java
@@ -13,15 +13,41 @@ public class Proyecto3 {
         System.out.print("Ingrese un numero entero: ");
         numero=entrada.nextInt();
         
-        System.out.println("El numero es: "+numero);
+        System.out.println("El numero entero es: "+numero);
         
-        //Ingresar un numero decimal
+        //Ingresar un numero decimal flotante
         Scanner entrada2 = new Scanner(System.in);
         float numero2;
         
-        System.out.print("Ingrese un numero decimal: ");
+        System.out.print("Ingrese un numero flotante: ");
         numero2 = entrada2.nextFloat();
         
-        System.out.println("El numero decimal es: "+numero2);
+        System.out.println("El numero flotante es: "+numero2);
+        
+        // Ingresar un numero decimal double
+        Scanner entrada3 = new Scanner(System.in);
+        double numero3;
+        
+        System.out.print("Ingrese un numero decimal (double): ");
+        numero3 = entrada3.nextDouble();
+        
+        System.out.println("El numero double es: "+numero3);
+        
+        // Ingresar cadenas de texto
+        Scanner entrada4 = new Scanner(System.in);
+        String cadena;
+        
+        System.out.print("Ingrese una cadena de texto: ");
+        cadena = entrada4.nextLine();
+        System.out.println("La cadena es: "+cadena);
+        
+        // Ingresar un solo caracter
+        Scanner entrada5 = new Scanner(System.in);
+        char letra;
+        
+        System.out.print("Ingrese una letra: ");
+        letra = entrada5.next().charAt(0);
+        
+        System.out.println("La letra es: "+letra);
     }
 }
