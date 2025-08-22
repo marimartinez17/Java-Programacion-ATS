fe3be012aef5bd5b5008afdd730cf49dc02b6c07
María Martínez <mariadanielamartinezr@gmail.com>
2025-08-22 09:23:42 -0400

    Create 6.OpAsignacion.java

---
+++ 6.OpAsignacion.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto6;
+
+/**
+ *
+ * @author usuario
+ */
+public class Proyecto6 {
+
+    public static void main(String[] args) {
+        int numero = 10;
+        
+        numero += 5; //numero = numero + 5
+        //numero -=5;
+        //numero *=5;
+        //numero /=5;
+        
+        System.out.println("El numero es: "+numero);
+    }
+}
