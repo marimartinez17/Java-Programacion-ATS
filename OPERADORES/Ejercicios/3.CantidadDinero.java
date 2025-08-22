066316c6b506134c21242b1e2add7a14818d2317
María Martínez <mariadanielamartinezr@gmail.com>
2025-08-22 11:37:55 -0400

    Create 3.CantidadDinero.java

---
+++ OPERADORES/Ejercicios/3.CantidadDinero.java
@@ -0,0 +1,33 @@
+/* Ejercicio 3:
+Guillermo tiene N dólares. Luis tiene la mitad de lo que posee Guillermo.
+Juan tiene la mitad de lo que poseen Luis y Guillermo juntos. Hacer un programa
+que calcule e imprima la cantidad de dinero que tienen entre los tres.
+ */
+
+package com.mycompany.ejercicio3;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        float guillermo,luis,juan,total;
+        
+        // Entrada de datos
+        System.out.print("Ingrese la cantidad de dinero que posee Guillermo: ");
+        guillermo = entrada.nextFloat();
+        
+        luis = guillermo/2;
+        
+        juan = (guillermo + luis)/2;
+        
+        total = guillermo+luis+juan;
+        
+        System.out.println("Guillermo tiene : "+guillermo+"$");
+        System.out.println("Luis tiene: "+luis+"$");
+        System.out.println("Juan tiene: "+juan+"$");
+        System.out.println("Por lo tanto, los tres tienen : "+total+"$ en total.");
+    }
+}
