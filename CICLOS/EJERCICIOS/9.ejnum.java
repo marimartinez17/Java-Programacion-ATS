87de7a240ce54cefa5751c47bc3cb6023b9df82d
María Martínez <mariadanielamartinezr@gmail.com>
2025-10-12 15:35:22 -0400

    Create 9.ejnum.java

---
+++ CICLOS/EJERCICIOS/9.ejnum.java
@@ -0,0 +1,18 @@
+/*
+ * Escribir todos los números del 100 al 0 de 7 en 7
+ */
+
+package com.mycompany.num;
+
+/**
+ *
+ * @author usuario
+ */
+public class App {
+
+    public static void main(String[] args) {
+        for (int i=100; i >=0; i-=7){
+            System.out.println(i);
+        }
+    }
+}
