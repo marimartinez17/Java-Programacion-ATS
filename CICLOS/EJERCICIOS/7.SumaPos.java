8679f4d12130728e26a26446be0acf44377c9c41
María Martínez <mariadanielamartinezr@gmail.com>
2025-10-12 15:16:02 -0400

    Update 7.SumaPos.java

--- CICLOS/EJERCICIOS/7.SumaPos.java
+++ CICLOS/EJERCICIOS/7.SumaPos.java
@@ -10,21 +10,26 @@ import javax.swing.JOptionPane;
 public class App {
 
     public static void main(String[] args) {
-        int numero, suma = 0, contador = 0;
+        int numero, suma = 0, contador = 0, media;
         
         // Solicitar num inicial
-        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número positivo: "));
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número positivo\n(Ingrese uno negativo para terminar)"));
         
         while (numero >= 0) {
             // Acumular num ingresados
             suma+= numero;
+            
             // Contar num ingresados
             contador++;
-            // Mostrar contador y suma
-            JOptionPane.showMessageDialog(null, "Suma actual: "+suma+"\nNúmeros ingresados: "+contador);
+
             // Solicitar otro número
             numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese otro número positivo: "));
         }
-
+        
+        // Calcular media de los números ingresados
+        media = suma / contador;
+        
+        // Mostrar el resultado
+        JOptionPane.showMessageDialog(null, "La media de los números ingresados es: "+media);
     }
 }
