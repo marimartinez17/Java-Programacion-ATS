78f0103b9f5684c61411d86576dcc62a43dc9eb7
María Martínez <mariadanielamartinezr@gmail.com>
2025-11-06 23:23:29 -0400

    Add main class to perform arithmetic operations

---
+++ POO/2.Metodos/main.java
@@ -0,0 +1,16 @@
+
+package ClasesYObjetos;
+        
+public class main {
+    public static void main(String [] args){
+        Operacion op = new Operacion();
+        
+        op.leerNum();
+        op.sumar();
+        op.restar();
+        op.multiplicar();
+        op.dividir();
+        op.resultados();
+    }
+    
+}
