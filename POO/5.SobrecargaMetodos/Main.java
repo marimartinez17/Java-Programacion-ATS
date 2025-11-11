2735fc172a8db9b54d3569d4d1746b263411e5b9
María Martínez <mariadanielamartinezr@gmail.com>
2025-11-11 18:47:40 -0400

    Create Main.java

---
+++ POO/5.SobrecargaMetodos/Main.java
@@ -0,0 +1,12 @@
+
+package ClasesYObjetos;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona p1 = new Persona("Maria", 18);
+        p1.correr();
+        
+        Persona p2 = new Persona("32792066");
+        p2.correr(100);
+    }
+}
