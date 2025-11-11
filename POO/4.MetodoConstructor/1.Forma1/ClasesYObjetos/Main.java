848231f852323054d60245a65a1576b7107f022f
María Martínez <mariadanielamartinezr@gmail.com>
2025-11-11 18:23:12 -0400

    Add Main class to demonstrate Persona objects

---
+++ POO/4.MetodoConstructor/1.Forma1/ClasesYObjetos/Main.java
@@ -0,0 +1,14 @@
+
+package ClasesYObjetos;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Persona p1 = new Persona("Maria",18);
+        Persona p2 = new Persona("Alejandro", 21);
+
+        p1.mostrarDatos();
+        p2.mostrarDatos();
+    }
+
+}
